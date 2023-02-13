aeba848ed10c027afe5129e6cfff0150f2e6b995
Maciej-Orczykowski <orczykowski.maciej99@gmail.com>
2023-02-13 05:10:25 +0100

    Mobile-First

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,94 +1,16 @@
-const HEAD = (
-  <div
-    style={{
-      width: "50px",
-      height: "50px",
-      borderRadius: "100%",
-      border: "10px solid black",
-      position: "absolute",
-      top: "50px",
-      right: "-30px",
-    }}
-  />
-);
+import styles from "./HangmanDrawing.module.css";
 
-const BODY = (
-  <div
-    style={{
-      width: "2px",
-      height: "100px",
-      background: "black",
-      border: "8px solid black",
-      position: "absolute",
-      top: "115px",
-      right: "-3.5px",
-    }}
-  />
-);
+const HEAD = <div className={`${styles.Head}`} />;
 
-const RIGHT_ARM = (
-  <div
-    style={{
-      width: "70px",
-      height: "2px",
-      background: "black",
-      border: "8px solid black",
-      position: "absolute",
-      top: "150px",
-      right: "-82px",
-      rotate: "-30deg",
-      transformOrigin: "left bottom",
-    }}
-  />
-);
+const BODY = <div className={`${styles.Body}`} />;
 
-const LEFT_ARM = (
-  <div
-    style={{
-      width: "70px",
-      height: "2px",
-      background: "black",
-      border: "8px solid black",
-      position: "absolute",
-      top: "150px",
-      right: "7px",
-      rotate: "30deg",
-      transformOrigin: "right bottom",
-    }}
-  />
-);
+const RIGHT_ARM = <div className={`${styles.RightArm}`} />;
 
-const RIGHT_LEG = (
-  <div
-    style={{
-      width: "100px",
-      height: "3px",
-      background: "black",
-      border: "7px solid black",
-      position: "absolute",
-      top: "210px",
-      right: "-103px",
-      rotate: "60deg",
-      transformOrigin: "left bottom",
-    }}
-  />
-);
+const LEFT_ARM = <div className={`${styles.LeftArm}`} />;
 
-const LEFT_LEG = (
-  <div
-    style={{
-      width: "100px",
-      height: "3px",
-      background: "black",
-      border: "7px solid black",
-      position: "absolute",
-      top: "210px",
-      right: "-1px",
-      rotate: "-60deg",
-      transformOrigin: "right bottom",
-    }}
-  />
-);
+const RIGHT_LEG = <div className={`${styles.RightLeg}`} />;
+
+const LEFT_LEG = <div className={`${styles.LeftLeg}`} />;
 
 const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
 
@@ -105,7 +27,7 @@ export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
           height: "50px",
           width: "10px",
           background: "black",
-          marginLeft: "120px",
+          marginRight: "75px",
           position: "absolute",
           top: 0,
           right: 0,
@@ -114,20 +36,27 @@ export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
       <div
         style={{
           height: "10px",
-          width: "200px",
+          width: "100px",
           background: "black",
-          marginLeft: "120px",
+          marginLeft: "73px",
         }}
       />
       <div
         style={{
-          height: "400px",
+          height: "280px",
           width: "10px",
           background: "black",
-          marginLeft: "120px",
+          marginLeft: "73px",
         }}
       />
-      <div style={{ height: "10px", width: "250px", background: "black" }} />
+      <div
+        style={{
+          height: "10px",
+          width: "150px",
+          background: "black",
+          marginRight: "100px",
+        }}
+      />
     </div>
   );
 }
